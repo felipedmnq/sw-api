e025588db7d9c03e5493bea35516ea5ffcd0fcf4
felipevscls <felipe.vasconcelos@vividgames.com>
2022-12-15 09:00:45 +0100

    tests

--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -8,5 +8,8 @@ class HTTPRequester(Extractor):
     def __init__(self) -> None:
         self.session = self.create_session()
 
-   
-   
+    def request_data(self, url: str) -> Dict[str, any]:
+
+        response = self.session.get(url)
+
+        return response.json()
